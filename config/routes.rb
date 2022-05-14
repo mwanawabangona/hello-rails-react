5297d5ffa4700445db4accc5a1199033b5f9c46e
mwanawabangona <ellisallday@gmail.com>
2022-05-14 12:34:22 +0200

    Add daatabase and react app inside rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+ namespace :v1, defaults: { format: :json } do
+  get 'greetings', to: 'greetings#index'
+ end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+ get '*path', to: 'static#index', constraints: -> (request) do
+  !request.xhr? && request.format.html?
+ end
+
+ root 'static#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Message.create(text:'Hola')
+Message.create(text:'Hoi')
+Message.create(text:'Habari')
+Message.create(text:'muli buanji')
+Message.create(text:'Salaam')
